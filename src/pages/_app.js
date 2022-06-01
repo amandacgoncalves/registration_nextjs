667d32635981fd64d046e2f0943d9ce7350ce423
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-06-01 12:43:43 -0300

    starting

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
+/* import tailwindcss from './tailwindcss' */
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
 
 export default MyApp
