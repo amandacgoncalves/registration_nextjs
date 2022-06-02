cb94036ad5a46268a8a7cb0c502b0b693bae53ad
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-06-02 10:10:58 -0300

    table component #03

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,38 @@
-import Layout from "../components/Layout"
-import Tabela from "../components/Tabela"
-import Cliente from "../core/Cliente"
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
-
   const clientes = [
-    new Cliente('Amanda', 19, '1'),
-    new Cliente('Ana', 26, '4'),
-    new Cliente('Bia', 12, '6'),
-    new Cliente('João', 19, '3'),
-  ]
+    new Cliente("Amanda", 19, "1"),
+    new Cliente("Ana", 26, "4"),
+    new Cliente("Bia", 12, "6"),
+    new Cliente("João", 19, "3"),
+  ];
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome);
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`Excluir... ${cliente.nome}`);
+  }
 
   return (
-    <div className={`
+    <div
+      className={`
       flex justify-center items-center h-screen
       bg-gradient-to-r from-blue-500 to-purple-500
       text-white
-    `}>
+    `}
+    >
       <Layout titulo="Cadastro Simples">
-        <Tabela clientes={clientes}></Tabela>
+        <Tabela
+          clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        />
       </Layout>
     </div>
-  )
+  );
 }
