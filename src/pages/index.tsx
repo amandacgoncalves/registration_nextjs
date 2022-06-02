4546456a4af5445af1d6cfd0db511430e27efe2d
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-06-02 08:58:45 -0300

    client class

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,12 @@ export default function Home() {
   return (
     <div className={`
       flex justify-center items-center h-screen
-      bg-gradient-tor-r from-blue-500 to-purple-500
+      bg-gradient-to-r from-blue-500 to-purple-500
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
         <span>Conteúdo</span>
       </Layout>
-     <span className="text-4xl">App</span>
     </div>
   )
 }
