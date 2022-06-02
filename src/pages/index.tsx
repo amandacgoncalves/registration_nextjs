4801573d7c5e253958898976e1ab1214752254ae
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-06-02 10:33:45 -0300

    button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
+import Botao from "../components/Botao";
 
 export default function Home() {
   const clientes = [
@@ -27,6 +28,9 @@ export default function Home() {
     `}
     >
       <Layout titulo="Cadastro Simples">
+        <div className="flex justify-end">
+        <Botao cor="green" className="mb-4">Novo Cliente</Botao>
+        </div>
         <Tabela
           clientes={clientes}
           clienteSelecionado={clienteSelecionado}
